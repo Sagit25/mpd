fba300d1f296ea110e7ab2e100829db8a388d1d1
Sukhun Yang <sukhuny@snu.ac.kr>
2024-06-01 07:36:54 +0000

    update dropdown

---
+++ src/components/Table.jsx
@@ -0,0 +1,13 @@
+import './Table.css';
+import {useEffect, useState} from "react";
+
+const Table = props => {
+    const [searchKey, setSearchKey] = useState(props.searchWord);
+
+    return (
+        <>
+        </>
+    );
+}
+
+export default Table;
