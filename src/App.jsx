88eec8f0502e3f1a11b1362c119fb209e058736f
Sukhun Yang <sukhuny@snu.ac.kr>
2024-06-01 05:56:39 +0000

    drop down

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,69 @@
 import './App.css';
+import Body from './components/Body.jsx';
+import Dropdown from './components/Dropdown';
+import {useState} from "react";
 
-function App() {
+const App = props => {
+  const [dropdownVisibility1, setDropdownVisibility1] = useState(false);
+  const [dropdownVisibility2, setDropdownVisibility2] = useState(false);
+  const [dropdownVisibility3, setDropdownVisibility3] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+      <div className='search-bar'>
+        <img src='./resources/logo.png'></img>
+        <div id='app'>
+            <button onClick={e => setDropdownVisibility1(!dropdownVisibility1)}>
+                군종
+            </button>
+            <Dropdown visibility={dropdownVisibility1}>
+                <ul>
+                    <li>육군</li>
+                    <li>해군</li>
+                    <li>공군</li>
+                    <li>부사관</li>
+                    <li>장교</li>
+                </ul>
+            </Dropdown>
+        </div>
+        <div id='app'>
+            <button onClick={e => setDropdownVisibility2(!dropdownVisibility2)}>
+                입대일
+            </button>
+            <Dropdown visibility={dropdownVisibility2}>
+                <ul>
+                    <li>
+                        <input placeholder='2024.03.04'/> 
+                        부터
+                    </li>
+                    <li>
+                        <input placeholder='2025.09.03'/> 
+                        까지
+                    </li>
+                </ul>
+            </Dropdown>
+        </div>
+        <div id='app'>
+            <button onClick={e => setDropdownVisibility3(!dropdownVisibility3)}>
+                전역일
+            </button>
+            <Dropdown visibility={dropdownVisibility3}>
+                <ul>
+                    <li>
+                        <input placeholder='2024.03.04'/> 
+                        부터
+                    </li>
+                    <li>
+                        <input placeholder='2025.09.03'/> 
+                        까지
+                    </li>
+                </ul>
+            </Dropdown>
+        </div>
+      </div>
+      <div className='main-view'> 
+        <Body />
+      </div>
     </div>
   );
 }
